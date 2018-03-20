20574116aa56657728bb97bef1f681252169056c
Ilia Kuzmin <i.kyzmin.v@gmail.com>
2018-03-20 23:59:14 +0400

    added fuzzy c-means and new backprop algo

--- main.py
+++ main.py
@@ -1,176 +1,109 @@
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
+import skfuzzy as fuzz
+from random import *
+import math
 
 
-class Network:
-    # layers -list [5 10 10 5] - 5 input, 2 hidden
-    # layers (10 neurons each), 5 output
-
-    def create(self, layers):
-        theta = [0]
-        # for each layer from the first (skip zero layer!)
-        for i in range(1, len(layers)):
-            # create nxM+1 matrix (+bias!) with random floats in range [-1; 1]
-            theta.append(
-                np.mat(np.random.uniform(-0.0001, 0.0001, (layers[i], layers[i - 1] + 1))))
-        nn = {'theta': theta, 'structure': layers}
-        return nn
-
-    def runAll(self, nn, X):
-        z = [0]
-        m = len(X)
-        a = [copy.deepcopy(X)]  # a[0] is equal to the first input values
-        logFunc = self.logisticFunctionVectorize()
-        # for each layer except the input
-        for i in range(1, len(nn['structure'])):
-            # add bias column to the previous matrix of activation functions
-            a[i - 1] = np.c_[np.ones(m), a[i - 1]]
-            # for all neurons in current layer multiply corresponds neurons
-            z.append(a[i - 1] * nn['theta'][i].T)
-            # in previous layers by the appropriate weights and sum the
-            # productions
-            a.append(logFunc(z[i]))  # apply activation function for each value
-        nn['z'] = z
-        nn['a'] = a
-        return a[len(nn['structure']) - 1]
-
-    def run(self, nn, input):
-        z = [0]
-        a = []
-        a.append(copy.deepcopy(input))
-        a[0] = np.matrix(a[0]).T  # nx1 vector
-        logFunc = self.logisticFunctionVectorize()
-        for i in range(1, len(nn['structure'])):
-            a[i - 1] = np.vstack(([1], a[i - 1]))
-            z.append(nn['theta'][i] * a[i - 1])
-            a.append(logFunc(z[i]))
-        nn['z'] = z
-        nn['a'] = a
-        return a[len(nn['structure']) - 1]
-
-    def logisticFunction(self, x):
-        a = 1 / (1 + np.exp(-x))
-        if a == 1:
-            a = 0.99999  # make smallest step to the direction of zero
-        elif a == 0:
-            a = 0.00001  # It is possible to use np.nextafter(0, 1) and
-        # make smallest step to the direction of one, but sometimes this step
-        # is too small and other algorithms fail :)
-        return a
-
-    def logisticFunctionVectorize(self):
-        return np.vectorize(self.logisticFunction)
-
-    def costTotal(self, theta, nn, X, y, lamb):
-        m = len(X)
-        # following string is for fmin_cg computaton
-        if type(theta) == np.ndarray:
-            nn['theta'] = self.roll(theta, nn['structure'])
-        y = np.matrix(copy.deepcopy(y))
-        # feed forward to obtain output of neural network
-        hAll = self.runAll(nn, X)
-        cost = self.cost(hAll, y)
-        # apply regularization
-        return cost / m + (lamb / (2 * m)) * self.regul(nn['theta'])
-
-    def cost(self, h, y):
-        logH = np.log(h)
-        log1H = np.log(1 - h)
-        # transpose y for matrix multiplication
-        cost = -1 * y.T * logH - (1 - y.T) * log1H
-        # sum matrix of costs for each output neuron and input vector
-        return cost.sum(axis=0).sum(axis=1)
-
-    def regul(self, theta):
-        reg = 0
-        thetaLocal = copy.deepcopy(theta)
-        for i in range(1, len(thetaLocal)):
-            # delete bias connection
-            thetaLocal[i] = np.delete(thetaLocal[i], 0, 1)
-            # square the values because they can be negative
-            thetaLocal[i] = np.power(thetaLocal[i], 2)
-            # sum at first rows, than columns
-            reg += thetaLocal[i].sum(axis=0).sum(axis=1)
-        return reg
-
-    def backpropagation(self, theta, nn, X, y, lamb):
-        layersNumb = len(nn['structure'])
-        thetaDelta = [0] * (layersNumb)
-        m = len(X)
-        # calculate matrix of outpit values for all input vectors X
-        hLoc = copy.deepcopy(self.runAll(nn, X))
-        yLoc = np.matrix(y)
-        thetaLoc = copy.deepcopy(nn['theta'])
-        derFunct = np.vectorize(
-            lambda x: (1 / (1 + np.exp(-x))) * (1 - (1 / (1 + np.exp(-x)))))
-
-        zLoc = copy.deepcopy(nn['z'])
-        aLoc = copy.deepcopy(nn['a'])
-        for n in range(0, len(X)):
-            delta = [0] * (layersNumb + 1)  # fill list with zeros
-            # calculate delta of error of output layer
-            delta[len(delta) - 1] = (hLoc[n].T - yLoc[n].T)
-            for i in range(layersNumb - 1, 0, -1):
-                # we can not calculate delta[0] because we don't have theta[0]
-                # (and even we don't need it)
-                if i > 1:
-                    z = zLoc[i - 1][n]
-                    # add one for correct matrix multiplication
-                    z = np.c_[[[1]], z]
-                    delta[i] = np.multiply(
-                        thetaLoc[i].T * delta[i + 1], derFunct(z).T)
-                    delta[i] = delta[i][1:]
-                thetaDelta[i] = thetaDelta[i] + delta[i + 1] * aLoc[i - 1][n]
-
-        for i in range(1, len(thetaDelta)):
-            thetaDelta[i] = thetaDelta[i] / m
-            thetaDelta[i][:, 1:] = thetaDelta[i][:, 1:] + \
-                                   thetaLoc[i][:, 1:] * (lamb / m)  # regularization
-
-        if type(theta) == np.ndarray:
-            # to work also with fmin_cg
-            return np.asarray(self.unroll(thetaDelta)).reshape(-1)
-        return thetaDelta
-
-    # create 1d array form lists like theta
-    def unroll(self, arr):
-        for i in range(0, len(arr)):
-            arr[i] = np.matrix(arr[i])
-            if i == 0:
-                res = (arr[i]).ravel().T
-            else:
-                res = np.vstack((res, (arr[i]).ravel().T))
-        res.shape = (1, len(res))
-        return res
-
-    # roll back 1d array to list with matrices according to given structure
-
-    def roll(self, arr, structure):
-        rolled = [arr[0]]
-        shift = 1
-        for i in range(1, len(structure)):
-            temparr = copy.deepcopy(
-                arr[shift:shift + structure[i] * (structure[i - 1] + 1)])
-            temparr.shape = (structure[i], structure[i - 1] + 1)
-            rolled.append(np.matrix(temparr))
-            shift += structure[i] * (structure[i - 1] + 1)
-        return rolled
-
-#https://home.deib.polimi.it/matteucc/Clustering/tutorial_html/cmeans.html
-class Fuzzy:
-    def init(self, data_for_clustering):
-        centers = np.mat(len(data_for_clustering))
-        clusters = np.empty(shape=[data_for_clustering.shape[0], data_for_clustering.shape[1]])
-        fn = {'data': data_for_clustering, 'clusters': clusters, 'centers': centers, 'm': 2, 'eps': 0.01}
-        return fn
-
-    def clusterize(self, fn):
-        # TODO clasterization
-        pass
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
 
 
-nt = Network()
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+    activation = weights[-1]
+    inputsList = np.asmatrix(inputs).tolist()[0]
+    for i in range(len(weights) - 1):
+        activation += weights[i] * inputsList[i]
+    return activation
+
+
+# Transfer neuron activation
+def transfer(activation):
+    return 1.0 / (1.0 + math.exp(-activation))
+
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+
+# Calculate the derivative of an neuron output
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network) - 1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+
+# Update network weights with error
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta']
+
+
+# Train a network for a fixed number of epochs
+def train_network(network, train, output, l_rate, n_epoch, n_outputs):
+    costs = list()
+    for epoch in range(n_epoch):
+        sum_error = 0
+        rowIndex = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = output[rowIndex].tolist()[0]
+            sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+            rowIndex += 1
+        print('>epoch=%d, lrate=%.3f, error=%.10f' % (epoch, l_rate, sum_error))
+        costs.append(sum_error)
+    return costs
+
+
+def predict(network, row):
+    outputs = forward_propagate(network, row)
+    return outputs
+
+
+seed(1)
 data = open("data.txt")
 raw_data = data.read().split("\n")
 inputRaw = []
@@ -182,28 +115,48 @@ for i in range(len(raw_data)):
     else:
         outputRaw.append(list(map(float, raw_data[i].split())))
 
-nn = nt.create([15, 1000, 15])
-
-lamb = 0.3
+# https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
+lamb = 1.850
 cost = 1
 alf = 0.002
 npInput = np.asmatrix(inputRaw)
 npOutput = np.asmatrix(outputRaw)
 iteration = 0
-x = list()
+fuzzyCost = list()
+perceptronCost = list()
 deltaAv = list()
-while (iteration < 1000):
-    average = 0
-    cost = nt.costTotal(False, nn, npInput[0:195], npOutput[0:195], lamb)
-    costTest = nt.costTotal(False, nn, npInput[196:205], npOutput[196:205], lamb)
-    delta = nt.backpropagation(False, nn, npInput[0:195], npOutput[0:195], lamb)
-    nn['theta'] = [nn['theta'][i] - alf * delta[i] for i in range(0, len(nn['theta']))]
-    print('Train cost ', cost[0, 0], 'Test cost ', costTest[0, 0])
-    # print(nt.runAll(nn, npInput[196:205]))
-    iteration += 1
-    x.append(cost[0, 0])
-plt.plot(x)
-print('Input :\n ', npInput[205], '\n', 'Output :\n', npOutput[205], '\n', 'Output From network :\n',
-      nt.runAll(nn, npInput[205]))
+cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+    npInput.T, 15, 2, error=0.002, maxiter=1500, init=None)
+
+fuzzyInput = u.T
+n_inputs = len(fuzzyInput[0])
+n_outputs = len(np.asarray(npOutput)[0])
+network = initialize_network(n_inputs, 15, n_outputs)
+train_network(network, fuzzyInput[0:195], npOutput[0:195], lamb, 800, n_outputs)
+i = 196
+error = 0
+testErrorFuzzy = list()
+for row in fuzzyInput[196:206]:
+    prediction = predict(network, row)
+    error = sum([(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
+    print('Test error = ', error)
+    testErrorFuzzy.append(error)
+    i += 1
+plt.plot(testErrorFuzzy, 'r')
+
+n_inputs = len(np.asarray(npInput)[0])
+n_outputs = len(np.asarray(npOutput)[0])
+network = initialize_network(n_inputs, 15, n_outputs)
+train_network(network, npInput[0:195], npOutput[0:195], lamb, 800, n_outputs)
+i = 196
+testError = list()
+for row in npInput[196:206]:
+    prediction = predict(network, row)
+    error = sum([(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
+    print('Test error = ', error)
+    testError.append(error)
+    i += 1
+plt.plot(testError, 'g')
+plt.ylim([0, 0.0002])
 plt.show()
 data.close()
