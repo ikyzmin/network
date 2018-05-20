bc04be73df2bcaa3235840974d9a47905c43dbe3
Ilia Kuzmin <i.kyzmin.v@gmail.com>
2018-05-20 17:59:12 +0400

    added new experiments

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ import os
 # Initialize a network
 def initialize_network(n_inputs, n_hidden, n_outputs):
     network = list()
-    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    hidden_layer = [{'weights': [np.random.uniform() for i in range(n_inputs + 1)]} for j in range(n_hidden)]
     network.append(hidden_layer)
-    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    output_layer = [{'weights': [np.random.uniform() for i in range(n_hidden + 1)]} for j in range(n_outputs)]
     network.append(output_layer)
     return network
 
 
 def initialize_network_with_layers(layers):
     network = list()
-    # for each layer from the first (skip zero layer!)
-    for i in range(len(layers)-1):
-        # create nxM+1 matrix (+bias!) with random floats in range [-1; 1]
-        layer = [{'weights': [random() for j in range(layers[i] + 1)]} for j in range(layers[i+1])]
+    for i in range(len(layers) - 1):
+        layer = [{'weights': [np.random.uniform() for j in range(layers[i] + 1)]} for k in range(layers[i + 1])]
         network.append(layer)
     return network
 
@@ -90,10 +88,39 @@ def update_weights(network, row, l_rate):
             neuron['weights'][-1] += l_rate * neuron['delta']
 
 
+def update_weights_momentum(network, row, l_rate, momentum):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j] + momentum * neuron['prevDelta'][j]
+                neuron['prevDelta'][j] = neuron['delta']
+            neuron['weights'][-1] += l_rate * neuron['delta']
+
+
 # Train a network for a fixed number of epochs
 def train_network(network, train, output, l_rate, n_epoch, n_outputs):
     costs = list()
-    decay_rate = l_rate / n_epoch
+    for epoch in range(n_epoch):
+        sum_error = 0
+        rowIndex = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = output[rowIndex].tolist()[0]
+            sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+            rowIndex += 1
+        if epoch % 100 == 0:
+            print('>epoch=%d, lrate=%.10f, error=%.10f' % (epoch, l_rate, sum_error))
+        costs.append(sum_error)
+    return costs
+
+
+def train_network_with_momentum(network, train, output, l_rate, n_epoch, n_outputs):
+    costs = list()
     momentum = 0.8
     for epoch in range(n_epoch):
         sum_error = 0
@@ -120,6 +147,8 @@ seed(1)
 data = open("data.txt")
 dataOutputFuzzy = open("fuzzy_results.txt", 'w+')
 dataOutputMLP = open("MLP_results.txt", 'w+')
+dataOutputFuzzyMomentum = open("fuzzy_momentum_results.txt", 'w+')
+dataOutputMLPMomentum = open("MLP_momenum_results.txt", 'w+')
 raw_data = data.read().split("\n")
 inputRaw = []
 outputRaw = []
@@ -135,7 +164,7 @@ lamb = 1.850
 cost = 1
 alf = 0.002
 epochs = 800
-c_means_epochs = 1200
+c_means_epochs = 1500
 npInput = np.asmatrix(inputRaw)
 npOutput = np.asmatrix(outputRaw)
 iteration = 0
@@ -148,47 +177,97 @@ cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
 fuzzyInput = u.T
 n_inputs = len(fuzzyInput[0])
 n_outputs = len(np.asarray(npOutput)[0])
-network = initialize_network_with_layers([n_inputs, 30, 15, n_outputs])
+network = initialize_network_with_layers([n_inputs, 15, n_outputs])
 train_network(network, fuzzyInput[0:164], npOutput[0:164], lamb, epochs, n_outputs)
 i = 164
-error = 0
-testErrorFuzzy = list()
+errorFuzzy = 0
 for row in fuzzyInput[164:206]:
     prediction = predict(network, row)
-    error = sum([(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
-    print('Test error = ', error)
-    testErrorFuzzy.append(error)
-    plt.plot(list(row), npOutput[i].tolist()[0], 'go', markersize=3, label="Контрольная выборка")
-    plt.plot(list(row), prediction, 'ro', markersize=3, label="Данные персептрона")
+    errorFuzzy += sum([(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
+    plt.plot(list(npInput[i].tolist()[0]), npOutput[i].tolist()[0], 'go', markersize=3, label="Контрольная выборка")
+    plt.plot(list(npInput[i].tolist()[0]), prediction, 'ro', markersize=3, label="Данные персептрона")
     dataOutputFuzzy.write(
         "Ожидаемый :\n\r" + str(npOutput[i].tolist()[0]) + "\n\rПолученный:\n\r" + str(prediction) + "\n\r")
     i += 1
+print('Fuzzy test error = %.10f' % errorFuzzy)
+
 plt.legend()
 plt.show()
 
 n_inputs = len(np.asarray(npInput)[0])
 n_outputs = len(np.asarray(npOutput)[0])
-network = initialize_network_with_layers([n_inputs, 30, 15, n_outputs])
+network = initialize_network_with_layers([n_inputs, 15, n_outputs])
 train_network(network, npInput[0:164], npOutput[0:164], lamb, epochs, n_outputs)
 i = 164
-testError = list()
+error = 0
 for row in np.asarray(npInput[164:206]):
     prediction = predict(network, row)
-    error = sum([(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
-    print('Test error = ', error)
-    testError.append(error)
+    error += sum([(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
     plt.plot(list(row), npOutput[i].tolist()[0], 'go', markersize=3, label="Контрольная выборка")
     plt.plot(list(row), prediction, 'ro', markersize=3, label="Данные персептрона")
     dataOutputMLP.write(
         "Ожидаемый :\n\r" + str(npOutput[i].tolist()[0]) + "\n\rПолученный :\n\r" + str(prediction) + "\n\r")
     i += 1
+print('MLP test error = %.10f' % error)
+
 plt.legend()
 plt.show()
-plt.plot(testErrorFuzzy, 'r', label=" слой с - средних")
-plt.plot(testError, 'g', label="Классический персептрон")
-plt.ylim([0, 0.001])
+
+cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+    npInput.T, 15, 2, error=0.002, maxiter=c_means_epochs, init=None)
+
+fuzzyInput = u.T
+
+network = initialize_network_with_layers([n_inputs, 15, n_outputs])
+train_network_with_momentum(network, fuzzyInput[0:164], npOutput[0:164], lamb, epochs, n_outputs)
+i = 164
+errorFuzzyMomentum = 0
+for row in fuzzyInput[164:206]:
+    prediction = predict(network, row)
+    errorFuzzyMomentum += sum(
+        [(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
+    plt.plot(list(npInput[i].tolist()[0]), npOutput[i].tolist()[0], 'go', markersize=3, label="Контрольная выборка")
+    plt.plot(list(npInput[i].tolist()[0]), prediction, 'ro', markersize=3, label="Данные персептрона")
+    dataOutputFuzzyMomentum.write(
+        "Ожидаемый :\n\r" + str(npOutput[i].tolist()[0]) + "\n\rПолученный :\n\r" + str(prediction) + "\n\r")
+    i += 1
+
+print('Fuzzy momentum test error = %.10f' % errorFuzzyMomentum)
 plt.legend()
+plt.show()
+
+network = initialize_network_with_layers([n_inputs, 15, n_outputs])
+train_network_with_momentum(network, fuzzyInput[0:164], npOutput[0:164], lamb, epochs, n_outputs)
+i = 164
+errorMomentum = 0
+for row in np.asarray(npInput[164:206]):
+    prediction = predict(network, row)
+    errorMomentum += sum(
+        [(npOutput[i].tolist()[0][j] - prediction[j]) ** 2 for j in range(len(np.asarray(npOutput)[0]))])
+    plt.plot(list(row), npOutput[i].tolist()[0], 'go', markersize=3, label="Контрольная выборка")
+    plt.plot(list(row), prediction, 'ro', markersize=3, label="Данные персептрона")
+    dataOutputMLPMomentum.write(
+        "Ожидаемый :\n\r" + str(npOutput[i].tolist()[0]) + "\n\rПолученный :\n\r" + str(prediction) + "\n\r")
+    i += 1
+print('MLP Momentum  test error = %.10f' % errorMomentum)
+plt.legend()
+plt.show()
+
+bar_width = 0.25
+plt.bar(0, errorFuzzy, bar_width, label='Ошибка тестирования с нечетким слоем')
+plt.bar(3 * bar_width / 2, errorFuzzyMomentum, bar_width, label='Ошибка тестирования с нечетким слоем и моментом')
+plt.bar(3 * bar_width, error, bar_width, label='Ошибка тестирования персептрона')
+plt.bar(9 * bar_width / 2, errorMomentum, bar_width, label='Ошибка тестирования персептрона с моментом')
+plt.ylabel('Погрешность')
+plt.xlabel('Персептрон')
+plt.title('Погрешность тестирования сетей')
+plt.xticks((0, 3 * bar_width / 2, 3 * bar_width, 9 * bar_width / 2),
+           ('Нечеткий слой', 'Классический', 'Нечеткий с моментом', "Классический с моментом"))
+plt.legend()
+plt.tight_layout()
 plt.show()
 data.close()
 dataOutputFuzzy.close()
 dataOutputMLP.close()
+dataOutputFuzzyMomentum.close()
+dataOutputMLPMomentum.close()
